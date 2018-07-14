ee8462001b34b6f8275b5ff5be6649913dbee551
Aleksandar Nedaković <aleksaleks123@gmail.com>
2018-07-14 18:27:16 +0200

    Ispravljen bug
    Bug - Kada se pri brisanju klikne na stanje moze da se desi da se obrse tranzicija ispod stanja umesto njega

--- Implementacija/src/gge/model/Brisanje.java
+++ Implementacija/src/gge/model/Brisanje.java
@@ -26,31 +26,26 @@ public class Brisanje extends StanjeAplikacije {
 		}
 
 		ElementDokumenta elementZaBrisanje = null;
-		Boolean stanje = false;
-		Boolean tranzicija = false;
 
 		for (Object value : noviDokument.getElementiDokumenta().keySet()) {
 			if (noviDokument.getElementiDokumenta().get(value) instanceof Stanje
 					&& ((Stanje) noviDokument.getElementiDokumenta().get(value)).getPravougaonik()
 							.contains(e.getPoint())) {
 				elementZaBrisanje = (Stanje) noviDokument.getElementiDokumenta().get(value);
-				stanje = true;
-				break;
-			} else if (noviDokument.getElementiDokumenta().get(value) instanceof Tranzicija) {
-				Rectangle2D rec = new Rectangle2D.Double(e.getPoint().getX() - 2, e.getPoint().getY() - 2, 4, 4);
+				this.aplikacija.obrisiStanje((Stanje) elementZaBrisanje, dokument);
+				return;
+			}
+		}
+		Rectangle2D rec = new Rectangle2D.Double(e.getPoint().getX() - 2, e.getPoint().getY() - 2, 4, 4);
+		for (Object value : noviDokument.getElementiDokumenta().keySet()) {
+			if (noviDokument.getElementiDokumenta().get(value) instanceof Tranzicija) {
 				if (((Tranzicija) noviDokument.getElementiDokumenta().get(value)).getLinija().intersects(rec)
 						|| ((Tranzicija) noviDokument.getElementiDokumenta().get(value)).getKrug().intersects(rec)) {
 					elementZaBrisanje = (Tranzicija) noviDokument.getElementiDokumenta().get(value);
-					tranzicija = true;
-					break;
+					this.aplikacija.obrisiTranziciju((Tranzicija) elementZaBrisanje, dokument);
+					return;
 				}
 			}
-		}
-
-		if (stanje) {
-			this.aplikacija.obrisiStanje((Stanje) elementZaBrisanje, dokument);
-		} else if (tranzicija) {
-			this.aplikacija.obrisiTranziciju((Tranzicija) elementZaBrisanje, dokument);
 		}
 	}
 
